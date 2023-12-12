f2afa3857889fa9e5e753d3337f8d6fcd3520678
deeprajgole26 <152276579+deeprajgole26@users.noreply.github.com>
2023-12-12 16:08:19 +0000

    objects and array

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,38 @@
+// singleton :- if any of constructor use it makes singleton object.
+// jb bhi hm literal ki trh declare karte he to singleton nahi banta he
+//Object.create
+
+// object literals 
+
+const mySym = Symbol("key1")
+
+const JsUser = {
+    name:"Deepraj",
+    "full name": "deepraj gole",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Indore",
+    email: "deeprajgole@gmail.com",
+    isLoggedIn: false,
+    isLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+
+JsUser.email = "deeprajgole@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "deeprajgole@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`); // string interpolation by using backstick(``)
+}
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
