ddd378f92d58a815a1d3e81d6c3ae12c8133a125
deeprajgole26 <152276579+deeprajgole26@users.noreply.github.com>
2023-12-12 19:28:00 +0530

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,6 +1,8 @@
 const name = "deepraj"
 const repoCount = 50
 
+console.log(name +repoCount + "Value");
+
 // console.log(name +repoCount + " Value");
 
 // console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
@@ -32,5 +34,4 @@ console.log(url.replace('%20','-'));
 console.log(url.includes('deep'));
 
 console.log(gameName.split('-'));
-
 
