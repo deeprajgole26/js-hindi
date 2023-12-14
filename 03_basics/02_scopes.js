640bf955a66618df114bad2a693d82b894fabd5d
deeprajgole26 <152276579+deeprajgole26@users.noreply.github.com>
2023-12-14 11:57:13 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,53 @@
+// var c = 300
+let a = 400
+if (true) {
+    let a = 10
+    const b = 20
+    //console.log("Inner :",a);  
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "deepraj"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
+if (true) {
+    const username = "deepraj"
+    if (username === "deepraj") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+// --------------------- Intresting -----------------
+
+console.log(addone(5))
+function addone(num) {
+    return num + 1
+}
+
+
+const addTwo = function (num) {
+    return num + 2
+    console.log(addTwo());
+}
+addTwo(5)
+// console.log(addTwo(5));
